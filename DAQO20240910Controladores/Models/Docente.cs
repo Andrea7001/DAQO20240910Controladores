0bd2297aaa18d2fee41aed0f507530a0c1fb9e41
Andrea7001 <qo24001@esfe.agape.edu.sv>
2024-09-10 18:10:04 -0600

    Agregar el modelo

---
+++ DAQO20240910Controladores/Models/Docente.cs
@@ -0,0 +1,14 @@
+﻿namespace DAQO20240910Controladores.Models
+{
+    public class Docente
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public string? Materia { get; set; }
+    }
+
+}
